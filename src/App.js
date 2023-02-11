d5bcaa891c3dc84ed4fcc00f20d31b026d00bc9e
Zin Mgmg <zinmgmg44868@gmail.com>
2023-02-11 15:58:54 +0630

    basic redux

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import React from "react";
-import { Container } from "./styles/Shares";
-import { GlobalCss } from "./styles/GlobalCSS";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import LoginContextProvider from "./store/LoginContentApi";
-import RouteGuard from "./helpers/RouteGuard";
-
-const theme = {
-  colors: {
-    primary: "aliceblue"
-  }
-}
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./store/actionCreators";
 
 function App() {
+  const logged = useSelector(state => state.loggedIn)
+  const dispatch = useDispatch()
+  const { login, logout } = bindActionCreators(actionCreators, dispatch)
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalCss />
-        <Container>
-          <BrowserRouter>
-            <LoginContextProvider>
-              <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/home" element={
-                  <RouteGuard>
-                    <Home />
-                  </RouteGuard>
-                } />
-              </Routes>
-            </LoginContextProvider>
-          </BrowserRouter>
-        </Container>
-      </ThemeProvider>
+      <h1>{logged ? "Member" : "Guest"}</h1>
+      <button onClick={() => login(true)}>Login </button>
+      <button onClick={() => logout(false)}>Logout</button>
     </>
   );
 }
